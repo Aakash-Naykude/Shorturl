40af81a7e71f0d2baa6cfee4798569406c734dd5
Aakash-Naykude <aakashnaykude123@gmail.com>
2022-02-26 19:02:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 const shortUrlController = require("./src/controller/short.controller");
 app.use("/short", shortUrlController);
 
